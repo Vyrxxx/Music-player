6564cfd67d11a9e2a24be6c0e9cda6614253843b
Vyrx <mylesspams@gmail.com>
2026-09-18 21:12:26 +0100

    Added Musicplayer.py
    Music Player what is currently very basic

---
+++ Musicplayer.py
@@ -0,0 +1,84 @@
+from playsound3 import playsound
+import easygui
+import os
+import random
+from pytubefix import YouTube
+
+current_playlist = None
+playlist = []
+
+while True:
+    
+    
+
+    os.path.exists(f"{current_playlist}")
+    playlist = [] 
+    if os.path.exists(f"{current_playlist}"):
+      with open(f"{current_playlist}", "r") as file:
+        for line in file:
+            playlist.append(line.strip())
+    else:
+     ignore = True
+    os.system("pause")
+    os.system("cls")
+    option = input("""
+========================
+       MUSIC PLAYER
+========================
+0. Import playlist
+1. Play local song
+2. Download from YouTube
+3. Add song to playlist
+4. Play playlist
+5. View playlist
+6. Exit
+
+Choose an option:
+""")
+
+    if option == "0":
+        playlists = []
+        current_playlist = easygui.fileopenbox(title="Select playlist to load", multiple=False)
+        for song in playlists:
+            print(song)
+
+    if option == "1":
+        file_path = easygui.fileopenbox()
+        print(f"Playing {file_path}")
+        playsound(file_path)
+       
+       
+    if option == "2":
+        url = input("Enter the url: ")
+        yt = YouTube(url)
+        print(f"Downloading + {url}")
+        video = yt.streams.filter(only_audio=True).first()
+        out_file = video.download()
+
+        base, ext = os.path.splitext(out_file)
+        new_file = base + ".mp3"
+        os.rename(out_file, new_file)
+        print(f"Saved as {new_file}")
+        
+    if option == "3":
+        file_path = easygui.fileopenbox()
+        playlist.append(file_path)
+        with open(f"{current_playlist}", "w") as file:
+           for song in playlist:
+             file.write(song + "\n")
+
+   
+    if option == "4":
+        random.shuffle(playlist)
+        for song in playlist:
+            playsound(song)
+
+
+    if option == "5":
+        for song in playlist:
+            print(song)
+
+
+    if option == "6":
+        print("Closing...")
+        break
